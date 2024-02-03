5f38469d033442b83c136dedd871a053bbfeeac0
Ilia <Rubiliam2003@gmail.com>
2024-02-03 13:31:45 -0700

    Initial project setup with basic folder structure and constants

---
+++ common/constants.h
@@ -0,0 +1,12 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+const int MAX_REGISTRATION_ATTEMPTS = 5;
+const int PORT_RANGE_MIN = 27500;
+const int PORT_RANGE_MAX = 27999;
+
+// Protocol constants
+const char* const FAILURE = "FAILURE";
+const char* const SUCCESS = "SUCCESS";
+
+#endif // CONSTANTS_H
